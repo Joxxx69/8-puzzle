f3a224937b7951af3fab1691b7aa26427afa9402
Joxxx69 <santiago.gualotuna@hotmail.com>
2022-07-04 09:55:15 -0500

    cambios en las funciones print

--- puzzle.cpp
+++ puzzle.cpp
@@ -45,40 +45,32 @@ int manhattan(vector<vector<int>> a, int moves)
 	return dist;
 }
 
-bool isGoal(vector<vector<int>> a)
-{ // boolean to see if we've reached our goal state
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			if (a[i][j] != goal[i][j])
-				return 0; // if our puzzle tiles aren't same with the goal position. For eg. we want a[0][0] = 1, a[0][1] = 2, a[2][2] = 0, etc.
-		}
-	}
+bool isGoal(vector<vector<int>> a){ // fucion que verifica si ya se encuentra en el estado objetivo
+	return (a[0][0] == 0 && a[0][1] == 1 && a[0][2] == 2 && a[1][0] == 3 && a[1][1] == 4 && a[1][2] == 5 && a[2][0] == 6 && a[2][1] == 7 && a[2][2] == 8);
+	//for (int i = 0; i < 3; i++){
+	//	for (int j = 0; j < 3; j++){
+	//		if (a[i][j] != goal[i][j]){
+	//			return false;
+	//		}
+	//			//return 0; // if our puzzle tiles aren't same with the goal position. For eg. we want a[0][0] = 1, a[0][1] = 2, a[2][2] = 0, etc.
+	//	}
+	//}
 
-	return 1;
+	//return 1;
 }
 
-bool guardar(int i, int j) // boolean guardar para ver si la matriz es 3x3
-{
-	if (i >= 0 && i <= 2 && j >= 0 && j <= 2)
-		return true;
-
-	return false;
+bool guardar(int i, int j){ // boolean guardar para ver si la matriz es 3x3
+	return (i >= 0 && i <= 2 && j >= 0 && j <= 2);
 }
 
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, 1, -1};
 
-vector<vector<vector<int>>> vecinos(vector<vector<int>> a)
-{
+vector<vector<vector<int>>> vecinos(vector<vector<int>> a){
 	pair<int, int> pos; // vector de pares denominado "pos" (empareja un entero con otro entero)
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			if (a[i][j] == 0)
-			{
+	for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; j++){
+			if (a[i][j] == 0){
 				pos.first = i;
 				pos.second = j;
 				break;
@@ -87,13 +79,11 @@ vector<vector<vector<int>>> vecinos(vector<vector<int>> a)
 	}
 
 	vector<vector<vector<int>>> resp; // vector de "respuesta" para agregar los valores correctos empujando hacia atrás
-	for (int k = 0; k < 4; k++)
-	{
+	for (int k = 0; k < 4; k++){
 		int cx = pos.first;	 // para tener un seguimiento de los valores "i"
 		int cy = pos.second; // para tener un seguimiento de los valores "j"
 		vector<vector<int>> n = a;
-		if (guardar(cx + dx[k], cy + dy[k]))
-		{
+		if (guardar(cx + dx[k], cy + dy[k])){
 			swap(n[cx + dx[k]][cy + dy[k]], n[cx][cy]); // intercambio de matrices
 			resp.push_back(n);
 		}
@@ -104,68 +94,57 @@ vector<vector<vector<int>>> vecinos(vector<vector<int>> a)
 
 typedef pair<vector<vector<int>>, int> estado; // nuevo tipo definido llamado "estado" que es un vector de pares
 
-struct cmp
-{
-	bool operator()(estado &a, estado &b) // overloading the unary operator "()"
-	{
+struct cmp{
+	bool operator()(estado &a, estado &b){ // overloading the unary operator "()"
 		int am = manhattan(a.first, a.second);
 		int bm = manhattan(b.first, b.second);
 		return am < bm;
 	}
 };
 
-void print_path(vector<vector<int>> s)
-{
-	if (parent.count(s))	   // to see if our vector s is present in our map named "parent" which takes vectors in it for both key and value, and if it is present
+void print_path(vector<vector<int>> s){
+	if (parent.count(s)){	   // to see if our vector s is present in our map named "parent" which takes vectors in it for both key and value, and if it is present
 		print_path(parent[s]); // then print the path
+	}
 
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			printf("%d ", s[i][j]);
+	for (int i = 0; i < 3; i++){
+		cout << "| ";
+		for (int j = 0; j < 3; j++){
+			cout <<s[i][j]<<" | ";
 		}
 		cout << endl;
 	}
 	cout << endl;
-
 	return;
 }
 
-void print(vector<vector<int>> s)
-{
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			printf("%d ", s[i][j]);
-		}
-		cout << endl;
-	}
-}
+//void print(vector<vector<int>> s){
+//	for (int i = 0; i < 3; i++){
+//		for (int j = 0; j < 3; j++){
+//			printf("%d ", s[i][j]);
+//			cout << "valor: [" << i << "][" << j << "]:"<<s[i][j];
+//		}
+//		cout << endl;
+//	}
+//}
 
-void solve(vector<vector<int>> a, int moves)
-{
+void solve(vector<vector<int>> a, int moves){
 
 	priority_queue<estado, vector<estado>, cmp> Q; // Priority queue named "Q".
 	Q.push(estado(a, moves));
-	while (!Q.empty())
-	{
+	while (!Q.empty()){
 		vector<vector<int>> s = Q.top().first;
 		Q.pop();
 		visited[s] = true;
-		if (s == goal) // print(s)
-		{
+		if (s == goal){ // print(s)
 			print_path(s);
 			break;
 		}
 		vector<vector<vector<int>>> ns = vecinos(s);
 		vector<vector<vector<int>>>::iterator it;
-		for (it = ns.begin(); it != ns.end(); it++)
-		{
+		for (it = ns.begin(); it != ns.end(); it++){
 			vector<vector<int>> temp = *it;
-			if (!visit(temp))
-			{
+			if (!visit(temp)){
 				parent.insert(pair<vector<vector<int>>, vector<vector<int>>>(temp, s));
 				Q.push(estado(temp, moves + 1));
 			}
@@ -174,15 +153,11 @@ void solve(vector<vector<int>> a, int moves)
 	return;
 }
 
-int obtenerInversiones(int arr[])
-{
+int obtenerInversiones(int arr[]){
 	int contador = 0;
-	for (int i = 0; i < 7; i++)
-	{
-		for (int j = i + 1; j < 8; j++)
-		{
-			if (arr[j] && arr[i] && arr[i] > arr[j])
-			{
+	for (int i = 0; i < 7; i++){
+		for (int j = i + 1; j < 8; j++){
+			if (arr[j] && arr[i] && arr[i] > arr[j]){
 				contador++;
 			}
 		}
@@ -190,16 +165,20 @@ int obtenerInversiones(int arr[])
 	return contador;
 }
 
-bool existeSolucion(int inversiones)
-{
-	if (inversiones % 2 == 0)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	};
+//bool existeSolucion(int inversiones)
+//{
+//	if (inversiones % 2 == 0)
+//	{
+//		return true;
+//	}
+//	else
+//	{
+//		return false;
+//	};
+//}
+
+bool existeSolucion(int inversiones){
+	return (inversiones % 2 == 0);
 }
 
 int main()
