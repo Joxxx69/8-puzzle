70bb263b8f15634acd2362280e0beb145d91036e
JairoGarciaF <jairo.dgf@hotmail.com>
2022-07-22 17:37:38 -0500

    Aumento de comentarios

--- puzzle.cpp
+++ puzzle.cpp
@@ -23,35 +23,39 @@ vector<int> valoresNOdos;
 int manhattan(vector<vector<int>> arreglo, int movimientos)
 {
 	distancia = movimientos;
-	for (int i = 0; i < 3; i++){ // for loop para pasar por cada fila y columna (como matrices). [00,01,02,10,11,12,20,21,22] Usaremos más este bucle i,j.
-		for (int j = 0; j < 3; j++){
-			if (arreglo[i][j] != 0){ // No se cuenta a la ficha 0, que representa a la ficha vacía
+	for (int i = 0; i < 3; i++)
+	{ // for loop para pasar por cada fila y columna (como matrices). [00,01,02,10,11,12,20,21,22] Usaremos más este bucle i,j.
+		for (int j = 0; j < 3; j++)
+		{
+			if (arreglo[i][j] != 0)
+			{ // No se cuenta a la ficha 0, que representa a la ficha vacía
 				/*
 				 *La función de Manhattan es igual a |xf-x0| + |yf-y0|. Donde xf representa a la columna donde se debe encontrar el
-                 *valor consultado, yf a la fila donde se debe encontrar el valor. x0 y y0 son las coordenadas [columna][fila] donde se
-                 *encuentra el valor actual.
-                 *
-                 *Como se está utilizando una vector de vectores, el cual es dinamico y tiene una dimension de n*n --> cuadrada.
-                 *Esto se puede solucionar al saber que, una posición t se puede dividir en componentes de filas y columnas de acuerdo a:
-                 *
-                 *  fila = t / numero_total_columnas (entero).   columna =  t % número_total_columnas(entero).
-                 *                 *
-                 *Por cómo está estructurada la resolución del problema, el valor t correspondiente al número ficha de la posicion de la matriz restado 1
-                 *así que con ello, se obtendrá la posición esperada, con respecto a su valor de ficha.
-                 *
-                 *  Ejemplo: Ficha 1 está en posición 3 del arreglo fichas[]. La posición 3 será igual a: fila = (arreglo[i][j]-1)/3 = 1.
-                 *                                                                                        columna = (arreglo[i][j]) MOD 3 = 0.
-                 *                                                            Posición 3 de la matriz corresponde a (1; 0) de una matriz.
-                 *
-                 *  Ahora Ficha 1 debería estar en t = 1. Así que, la posición esperada es: fila = (arreglo[i][j]-1)/3 = 1.
-                 *                                                                          columna = (arreglo[i][j]) MOD 3 = 1.
-                 *																Posición esperada de t = 1: (1; 1).
-                 *
-                 */
+				 *valor consultado, yf a la fila donde se debe encontrar el valor. x0 y y0 son las coordenadas [columna][fila] donde se
+				 *encuentra el valor actual.
+				 *
+				 *Como se está utilizando una vector de vectores, el cual es dinamico y tiene una dimension de n*n --> cuadrada.
+				 *Esto se puede solucionar al saber que, una posición t se puede dividir en componentes de filas y columnas de acuerdo a:
+				 *
+				 *  fila = t / numero_total_columnas (entero).   columna =  t % número_total_columnas(entero).
+				 *                 *
+				 *Por cómo está estructurada la resolución del problema, el valor t correspondiente al número ficha de la posicion de la matriz restado 1
+				 *así que con ello, se obtendrá la posición esperada, con respecto a su valor de ficha.
+				 *
+				 *  Ejemplo: Ficha 1 está en posición 3 del arreglo fichas[]. La posición 3 será igual a: fila = (arreglo[i][j]-1)/3 = 1.
+				 *                                                                                        columna = (arreglo[i][j]) MOD 3 = 0.
+				 *                                                            Posición 3 de la matriz corresponde a (1; 0) de una matriz.
+				 *
+				 *  Ahora Ficha 1 debería estar en t = 1. Así que, la posición esperada es: fila = (arreglo[i][j]-1)/3 = 1.
+				 *                                                                          columna = (arreglo[i][j]) MOD 3 = 1.
+				 *																Posición esperada de t = 1: (1; 1).
+				 *
+				 */
 				int fila = (arreglo[i][j] - 1) / 3;
 				int columna = (arreglo[i][j] - 1) % 3;
 				distancia += abs(j - columna) + abs(i - fila);
-				if (valoresNOdos.size() < 8){
+				if (valoresNOdos.size() < 8)
+				{
 					valoresNOdos.push_back(abs(j - columna) + abs(i - fila));
 				}
 			}
@@ -64,16 +68,16 @@ bool guardar(int i, int j)
 { // boolean guardar para ver si la matriz es 3x3
 	return (i >= 0 && i <= 2 && j >= 0 && j <= 2);
 }
-//Lista de las posibles acciones con las posiciones matriciales 
-int dx[] = {-1, 1, 0, 0};// filas 
-int dy[] = {0, 0, 1, -1};// columnas
+// Lista de las posibles acciones con las posiciones matriciales
+int dx[] = {-1, 1, 0, 0}; // filas
+int dy[] = {0, 0, 1, -1}; // columnas
 
 vector<vector<vector<int>>> vecinos(vector<vector<int>> a)
 {
 	pair<int, int> pos; // vector de pares denominado "pos" (empareja un entero con otro entero)
 	for (int i = 0; i < 3; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < 3; j++) // asignacion de posiciones
 		{
 			if (a[i][j] == 0)
 			{
@@ -104,30 +108,33 @@ bool estadoObjetivoAlcanzado(vector<vector<int>> a)
 	return (a == estadoObjetivo);
 }
 
-void estadosPrint(vector<vector<int>> s){
-	if (hijos.count(s)){							// para ver si nuestro vector s está presente en nuestro mapa llamado "hijos" que toma vectores tanto para clave como para valor, y si está presente
+void estadosPrint(vector<vector<int>> s)
+{
+	if (hijos.count(s))
+	{							// para ver si nuestro vector s está presente en nuestro mapa llamado "hijos" que toma vectores tanto para clave como para valor, y si está presente
 		estadosPrint(hijos[s]); // luego imprima la ruta
-		int heuristicaEstado = (estadoObjetivoAlcanzado(s)) ? 0 : manhattan(s,0) ;
+		int heuristicaEstado = (estadoObjetivoAlcanzado(s)) ? 0 : manhattan(s, 0);
 		profundidad++;
-		cout << "g(n): " << profundidad<<" : ";
-		cout << "h(n): " << heuristicaEstado<<" : ";
+		// output datos Heurística
+		cout << "g(n): " << profundidad << " : ";
+		cout << "h(n): " << heuristicaEstado << " : ";
 		cout << "f(n):" << profundidad + heuristicaEstado;
 		cout << endl;
 	}
-	for (int i = 0; i < 3; i++){
+	// Impresión de mas matrices del puzzle
+	for (int i = 0; i < 3; i++)
+	{
 		cout << "| ";
-		for (int j = 0; j < 3; j++){
+		for (int j = 0; j < 3; j++)
+		{
 			cout << s[i][j] << " | ";
 		}
 		cout << endl;
 	}
 	cout << endl;
 
-
-
 	return;
 }
-
 
 bool visit(vector<vector<int>> a)
 { // esta funcion verifica si el nodo ya fue visitado
@@ -135,21 +142,21 @@ bool visit(vector<vector<int>> a)
 }
 typedef pair<vector<vector<int>>, int> estado; // nuevo tipo definido llamado "estado" que es un vector de pares
 
-
 /* Un predicado binario que toma dos elementos (de tipoT) como argumentos y devuelve un bool.
 La expresión comp(a,b), donde comp es un objeto de este tipo y a y b son elementos del contenedor,
-devolverá true si se considera que a va antes de b en el estricto ordenamiento débil que define la 
-función.los prioridad_cola utiliza esta función para mantener los elementos ordenados de una manera 
-que conserva las propiedades del montón (es decir, que el elemento extraído sea el último de acuerdo 
+devolverá true si se considera que a va antes de b en el estricto ordenamiento débil que define la
+función.los prioridad_cola utiliza esta función para mantener los elementos ordenados de una manera
+que conserva las propiedades del montón (es decir, que el elemento extraído sea el último de acuerdo
 con este estricto ordenamiento débil ). */
-struct comparador{  // este elem
-	bool operator()(estado &a, estado &b){ // sobrecargando el operador unario "()"
+struct comparador
+{ // este elem
+	bool operator()(estado &a, estado &b)
+	{ // sobrecargando el operador unario "()"
 		int am = manhattan(a.first, a.second);
 		int bm = manhattan(b.first, b.second);
 		return am < bm;
 	}
 };
-
 
 void encontrarSolucion(vector<vector<int>> a, int moves)
 {
@@ -158,11 +165,13 @@ void encontrarSolucion(vector<vector<int>> a, int moves)
 	colaP.push(estado(a, moves));
 	while (!colaP.empty())
 	{
+		// proceso de solución del puzzle
 		vector<vector<int>> aux = colaP.top().first;
 		colaP.pop();
 		visitados[aux] = true;
 		// if (aux == estadoObjetivo){ // print(s)
-		if (estadoObjetivoAlcanzado(aux)){ // print(s)
+		if (estadoObjetivoAlcanzado(aux))
+		{ // print(s)
 			estadosPrint(aux);
 			break;
 		}
@@ -181,6 +190,7 @@ void encontrarSolucion(vector<vector<int>> a, int moves)
 	return;
 }
 
+// Método para saber si el puzzle es resoluble
 int obtenerInversiones(int arr[])
 {
 	int contador = 0;
@@ -202,6 +212,7 @@ bool existeSolucion(int inversiones)
 	return (inversiones % 2 == 0);
 }
 
+// Metodo que compara los numeros repetidos
 bool repeticionDominio(int num, int arreglo[])
 {
 	for (int i = 0; i < 10; i++)
@@ -214,6 +225,7 @@ bool repeticionDominio(int num, int arreglo[])
 	return false;
 }
 
+// Generacion aleatoria de estados iniciales del puzzle
 int *ejemplosPuzzle(int arreglo[])
 {
 	srand(time(NULL));
@@ -223,7 +235,7 @@ int *ejemplosPuzzle(int arreglo[])
 		do
 		{
 			num = rand() % 9;
-		} while (repeticionDominio(num, arreglo));
+		} while (repeticionDominio(num, arreglo)); // asignacion de numeros aleatorios
 		arreglo[i] = num;
 	}
 	return arreglo;
@@ -231,12 +243,12 @@ int *ejemplosPuzzle(int arreglo[])
 
 int main()
 {
-
+	// Inicializacion del arreglo
 	int arreglo[9], arregloAux[8], k = 0, w = 0;
 	int *arregloEstados = ejemplosPuzzle(arreglo);
 	vector<vector<int>> estadoInicial(3, vector<int>(3));
 	for (int i = 0; i < 3; i++)
-	{
+	{ // Asignación del estado generado aleatoriamente al estado inicial
 		for (int j = 0; j < 3; j++)
 		{
 			estadoInicial[i][j] = arregloEstados[w];
@@ -251,41 +263,44 @@ int main()
 	cout << endl
 		 << "Estado Inicial: " << endl;
 	estadosPrint(estadoInicial);
-	int distanciaManhattan = manhattan(estadoInicial,0);
+	int distanciaManhattan = manhattan(estadoInicial, 0);
 
-	if (distanciaManhattan <= 10){
-		if(distanciaManhattan == 0){
+	if (distanciaManhattan <= 10)
+	{
+		if (distanciaManhattan == 0)
+		{
 			cout << "g(n)=0 : h(n)=0 : f(n)=0";
 			return 0;
 		}
-		cout << "El valor heuristico es de " << distanciaManhattan << endl;
+		cout << "El valor heuristico es de " << distanciaManhattan << endl; // Output dato del valor heurístico
 		if (existeSolucion(obtenerInversiones(arregloAux)))
 		{
+			// Verificación si existe solución
 			cout << "Numero de inversiones: " << obtenerInversiones(arregloAux) << ", por lo tanto existe solucion.\n"
 				 << endl;
 			cout << "\nResolviendo el puzzle, espere por favor...\n";
 			estadoObjetivo = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
 			cout << endl;
-			encontrarSolucion(estadoInicial, 0);
-			cout << "Distancia Manhattan: " << distanciaManhattan;
+			encontrarSolucion(estadoInicial, 0);				   // Resolución del puzzle
+			cout << "Distancia Manhattan: " << distanciaManhattan; // Output distancia Manhattan
 			cout << endl;
-			cout << "Costo de cada nodo: ";
+			cout << "Costo de cada nodo: "; // Output costo de cada nodo
 			for (int idx : valoresNOdos)
 			{
 				cout << idx << " ";
 			}
 			cout << endl;
-			cout << "Profundidad: " << profundidad << endl;
+			cout << "Profundidad: " << profundidad << endl; // Output dato de profundidad de la solución
 		}
 		else
-		{
+		{ // solución inexistente
 			cout << "Numero de inversiones: " << obtenerInversiones(arregloAux) << ", por lo tanto no existe solucion.\n"
 				 << endl;
 		};
 	}
 	else
-	{
-		cout << "El costo heuristico es " << manhattan(estadoInicial,0)<< ", por lo tanto es mayor a 10";
+	{ // Costo heurístico sobrepasado
+		cout << "El costo heuristico es " << manhattan(estadoInicial, 0) << ", por lo tanto es mayor a 10";
 	}
 	return 0;
 }
